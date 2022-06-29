3552a278376bf50768207dee3819dc7cb6c1c6f2
1210018 <cbhs1210018@gn.iwasaki.ac.jp>
2022-06-29 18:53:57 +0900

    terminated

---
+++ calcMethod/CalcM.js
@@ -0,0 +1,660 @@
+﻿let i = 0;
+let iS = 0;
+let A = new Array();
+let B = new Array();
+let Opr = 1;
+let OprS = 1;
+let Pare = 0;
+let W = 0;
+let WS = 0;
+let Y = 0;
+let Pr = 0;
+let BR;
+
+function onClick1() {
+    if (Pare == 0) {
+        A[i] = 1;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 1;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+function onClick2(){
+    if (Pare == 0) {
+        A[i] = 2;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 2;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+function onClick3(){
+    if (Pare == 0) {
+        A[i] = 3;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 3;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+
+function onClick4() {
+    if (Pare == 0) {
+        A[i] = 4;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 4;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+function onClick5() {
+    if (Pare == 0) {
+        A[i] = 5;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 5;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    } 
+}
+function onClick6() {
+    if (Pare == 0) {
+        A[i] = 6;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 6;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+
+function onClick7() {
+    if (Pare == 0) {
+        A[i] = 7;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 7;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+function onClick8() {
+    if (Pare == 0) {
+        A[i] = 8;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 8;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+function onClick9() {
+    if (Pare == 0) {
+        A[i] = 9;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10
+            }
+        }
+        i = i + 1;
+    } else {
+        B[iS] = 9;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10
+            }
+        }
+        iS = iS + 1;
+    }
+}
+
+function onClick0() {
+    if (Pare == 0) {
+        A[i] = 0;
+        if (Pr != 1) {
+            if (i != 0) {
+                for (let h = 0; h < i; h++) {
+                    A[h] = A[h] * 10;
+                }
+            }
+            i = i + 1;
+        } else {
+            for (let h = 0; h <= i; h++) {
+                A[i] = A[i] / 10;
+            }
+            i = i + 1;
+        }
+    } else {
+        B[iS] = 0;
+        if (Pr != 1) {
+            if (iS != 0) {
+                for (let h = 0; h < iS; h++) {
+                    B[h] = B[h] * 10;
+                }
+            }
+            iS = iS + 1;
+        } else {
+            for (let h = 0; h <= iS; h++) {
+                B[iS] = B[iS] / 10;
+            }
+            iS = iS + 1;
+        }
+    }
+}
+
+function onClickPr() {
+    Pr = 1;
+}
+
+function onClickP() {
+    if (Pare == 0) {
+        Y = 0
+        if (Opr == 1) {
+            for (let g = 0; g < i; g++) {
+                W = W + A[g];
+            }
+        }
+        if (Opr == 2) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W - Y;
+        }
+        if (Opr == 3) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W * Y;
+        }
+        if (Opr == 4) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W / Y;
+        }
+        i = 0;
+        Pr = 0;
+        Opr = 1;
+    } else {
+        Y = 0
+        if (OprS == 1) {
+            for (let g = 0; g < iS; g++) {
+                WS = WS + B[g];
+            }
+        }
+        if (OprS == 2) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS - Y;
+        }
+        if (OprS == 3) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS * Y;
+        }
+        if (OprS == 4) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS / Y;
+        }
+        iS = 0;
+        Pr = 0;
+        OprS = 1;
+    }
+}
+
+function onClickM() {
+    if (Pare == 0) {
+        Y = 0
+        if (Opr == 1) {
+            for (let g = 0; g < i; g++) {
+                W = W + A[g];
+            }
+        }
+        if (Opr == 2) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W - Y;
+        }
+        if (Opr == 3) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W * Y;
+        }
+        if (Opr == 4) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W / Y;
+        }
+        i = 0;
+        Pr = 0;
+        Opr = 2;
+    } else {
+        Y = 0
+        if (OprS == 1) {
+            for (let g = 0; g < iS; g++) {
+                WS = WS + B[g];
+            }
+        }
+        if (OprS == 2) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS - Y;
+        }
+        if (OprS == 3) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS * Y;
+        }
+        if (OprS == 4) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS / Y;
+        }
+        iS = 0;
+        Pr = 0;
+        OprS = 2;
+    }
+}
+
+function onClickT() {
+    if (Pare == 0) {
+        Y = 0
+        if (Opr == 1) {
+            for (let g = 0; g < i; g++) {
+                W = W + A[g];
+            }
+        }
+        if (Opr == 2) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W - Y;
+        }
+        if (Opr == 3) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W * Y;
+        }
+        if (Opr == 4) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W / Y;
+        }
+        i = 0;
+        Pr = 0;
+        Opr = 3;
+    } else {
+        Y = 0
+        if (OprS == 1) {
+            for (let g = 0; g < iS; g++) {
+                WS = WS + B[g];
+            }
+        }
+        if (OprS == 2) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS - Y;
+        }
+        if (OprS == 3) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS * Y;
+        }
+        if (OprS == 4) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS / Y;
+        }
+        iS = 0;
+        Pr = 0;
+        OprS = 3;
+    }
+}
+
+function onClickD() {
+    if (Pare == 0) {
+        Y = 0
+        if (Opr == 1) {
+            for (let g = 0; g < i; g++) {
+                W = W + A[g];
+            }
+        }
+        if (Opr == 2) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W - Y;
+        }
+        if (Opr == 3) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W * Y;
+        }
+        if (Opr == 4) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W / Y;
+        }
+        i = 0;
+        Pr = 0;
+        Opr = 4;
+    } else {
+        Y = 0
+        if (OprS == 1) {
+            for (let g = 0; g < iS; g++) {
+                WS = WS + B[g];
+            }
+        }
+        if (OprS == 2) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS - Y;
+        }
+        if (OprS == 3) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS * Y;
+        }
+        if (OprS == 4) {
+            for (let g = 0; g < iS; g++) {
+                Y = Y + B[g];
+            }
+            WS = WS / Y;
+        }
+        iS = 0;
+        Pr = 0;
+        OprS = 4;
+    }
+}
+
+function onClickPareS() {
+    Pare = 1;
+    WS = 0;
+    OprS = 1;
+    iS = 0;
+}
+
+function onClickPareE() {
+    Y = 0;
+    if (OprS == 1) {
+        for (let g = 0; g < iS; g++) {
+            WS = WS + B[g];
+        }
+    }
+    if (OprS == 2) {
+        for (let g = 0; g < iS; g++) {
+            Y = Y + B[g];
+        }
+        WS = WS - Y;
+    }
+    if (OprS == 3) {
+        for (let g = 0; g < iS; g++) {
+            Y = Y + B[g];
+        }
+        WS = WS * Y;
+    }
+    if (OprS == 4) {
+        for (let g = 0; g < iS; g++) {
+            Y = Y + B[g];
+        }
+        WS = WS / Y;
+    }
+    Pare = 0;
+    A[0] = WS;
+    i = 1;
+    Pr = 0;
+
+}
+
+function onClickAns() {
+    if (Pare == 0) {
+        Y = 0;
+        if (Opr == 1) {
+            for (let g = 0; g < i; g++) {
+                W = W + A[g];
+            }
+        }
+        if (Opr == 2) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W - Y;
+        }
+        if (Opr == 3) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W * Y;
+        }
+        if (Opr == 4) {
+            for (let g = 0; g < i; g++) {
+                Y = Y + A[g];
+            }
+            W = W / Y;
+        }
+        Ans.innerHTML = W;
+        i = 0;
+        Pr = 0;
+    }
+}
+
+function onClickAC() {
+    i = 0;
+    iS = 0;
+    Opr = 1;
+    OprS = 1;
+    Pare = 0;
+    W = 0;
+    WS = 0;
+    Pr = 0;
+}
+function onClickBear() {
+    for (let c = 1; c <= W;c++) {
+        BR = BR + "🧸";
+    }
+   Bear.innerHTML = BR;
+}
